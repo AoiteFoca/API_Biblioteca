ac20e1553fbed96b4addbb04243384165b0238d3
AoiteFoca <nathancielusinski@gmail.com>
2024-09-14 11:46:03 -0300

    Correções
    Agora o valor do status apresenta um Booleano e não None;
    A saída do dado foi trabalhada para, dependendo do booleano, apresentar ativo ou bloqueado.

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users(
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(50) NOT NULL,
     nomeCompleto VARCHAR(100) NOT NULL,
-    status BOOLEAN TRUE,
+    status BOOLEAN DEFAULT TRUE,
     dataCriacao TIMESTAMP DEFAULT (DATETIME(CURRENT_TIMESTAMP, '-3 hours')),
     dataAtualizacao TIMESTAMP DEFAULT (DATETIME(CURRENT_TIMESTAMP, '-3 hours'))
 );
