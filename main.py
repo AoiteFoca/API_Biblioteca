9f891af2b8dd7739499548636180736c7ab95be6
Nathan C <141975272+AoiteFoca@users.noreply.github.com>
2024-10-25 19:32:41 -0300

    PassRecover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask, request, redirect, url_for, session
+from flask import render_template, Flask, request, redirect, url_for, session, flash
 from routes.db import init_db, close_db
 from routes.users import users_bp
 import logging
@@ -6,6 +6,8 @@ import os
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.contrib.github import make_github_blueprint, github
 from dotenv import load_dotenv
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 
 
 #-------------------------Inicio---------------------------#
@@ -24,6 +26,21 @@ client_secret_github = os.getenv("CLIENT_SECRET_GITHUB")
 # Configurações de segurança
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'  # Permite o uso de http
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = '1'   # Permite o uso de escopos diferentes
+
+app.secret_key = os.getenv("APP_SECRET_KEY") #Senha para os cookies de sessão
+
+#Configuração do servidor de e-mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.getenv("APP_EMAIL")
+app.config['MAIL_PASSWORD'] = os.getenv("APP_PASSWORD")
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+#Serializador para gerar os tokens seguros
+serial = URLSafeTimedSerializer(app.secret_key)
 
 #----------------------Blueprints-----------------------#
 
@@ -102,6 +119,44 @@ def login():
 def perfil(): 
     user_data = session.get('user')
     return render_template('perfil.html', google_data=user_data)
+
+#-------------------------Perfil--------------------------#
+
+@app.route('/reset_password', methods=['POST', 'GET'])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        #Preparação e envio do e-mail
+        token = serial.dumps(email, salt='password_recovery')
+        msg = Message('Recuperação de senha', sender=os.getenv("APP_EMAIL"), recipients=[email])
+        link = url_for('reset_password', token=token, _external=True)
+        msg.body = f'Clique no link a seguir para redefinir sua senha: {link}'
+        mail.send(msg)
+
+        flash('Um link de recuperação de senha foi enviado para o seu e-mail.', category='success')
+
+        return redirect(url_for('home'))
+    
+    return render_template('reset_password.html')
+
+#Rota para redefinir a senha
+@app.route('/reset_password/<token>', methods=['POST', 'GET'])
+def reset_password_token(token):
+    try:
+        email = serial.loads(token, salt='password_recovery', max_age=3600)
+    except SignatureExpired:
+        flash('O link de recuperação de senha expirou.', category='error')
+        return redirect(url_for('reset_password'))
+    except BadSignature:
+        flash('Link inválido.', category='error')
+        return redirect(url_for('reset_password'))
+
+    if request.method == 'POST':
+        new_password = request.form['password']
+        flash('Senha alterada com sucesso.', category='success')
+        return redirect(url_for('home'))
+    return render_template('reset.html')
 
 #-------------------------Fim--------------------------#
 
